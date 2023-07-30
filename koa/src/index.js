934ac2bf0d6f27528620bb81fb307ea058187bae
wanghan <1594078165@qq.com>
2023-07-31 00:29:13 +0800

    feat: login

--- koa/src/index.js
+++ koa/src/index.js
@@ -1,26 +1,48 @@
 const Koa = require('koa')
-const app = new Koa()
+const Router = require('koa-router')
+const bodyParser = require('koa-bodyparser')
 
+const app = new Koa()
+const router = new Router()
+
+// 添加中间件
 app.use(async (ctx, next) => {
-    console.log('中间件1 开始处理')
-    await next()
-    console.log('中间件1 处理结束')
-})
-app.use(async (ctx, next) => {
-    console.log('中间件2 开始处理')
-    await next()
-    console.log('中间件2 处理结束')
-})
-app.use(async (ctx, next) => {
-    console.log('中间件3 开始处理')
-    await next()
-    console.log('中间件3 处理结束')
-})
-app.use(async ctx => {
-    ctx; // is the Context
-    ctx.request; // is a koa Request
-    ctx.response; // is a koa Response
+    ctx.set('Access-Control-Allow-Origin', '*');
+    ctx.set('Access-Control-Allow-Headers', '*');
+    await next();
 });
-app.listen(3000, () => {
-    console.log('server is listen in 3000~')
+app.use(bodyParser());
+app.use(router.routes());
+app.use(router.allowedMethods());
+// 假设这里使用了一个模拟数据库的 users 对象来存储用户信息
+const users = {
+    'user1': {
+      username: 'user1',
+      password: 'password1'
+    },
+    'user2': {
+      username: 'user2',
+      password: 'password2'
+    }
+};
+
+router.post('/login', (ctx) => {
+    const { username, password } = ctx.request.body
+    const user = users[username]
+    if (!user) {
+        ctx.status = 401
+        ctx.body = { message: 'User is Not Found' }
+        return;
+    }
+    if (user.password !== password) {
+        ctx.status = 401
+        ctx.body = { message: 'Error Password' }
+        return;
+    }
+    ctx.body = { username: user.username }
 })
+
+// 启动服务
+app.listen(3000, () => {
+    console.log('Server running on port 3000');
+});
